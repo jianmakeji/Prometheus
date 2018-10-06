dd88de8b06043b330f0ef0c1add41ebb0146f366
codeliling <maximuslee@126.com>
2018-10-06 10:13:26 +0800

    修改bug

--- Prometheus/app/controller/website/course.js
+++ Prometheus/app/controller/website/course.js
@@ -2,28 +2,52 @@
 
 const Controller = require('egg').Controller;
 
-class CourseTypeController extends Controller {
+class CourseController extends Controller {
   async index() {
     const ctx = this.ctx;
     const query = {
       limit: ctx.helper.parseInt(ctx.query.limit),
       offset: ctx.helper.parseInt(ctx.query.offset),
     };
-
-    ctx.body = await ctx.service.courseType.list(query);
+    ctx.body = await ctx.service.course.list(query);
   }
 
   async show() {
     const ctx = this.ctx;
-    ctx.body = await ctx.service.courseType.find(ctx.helper.parseInt(ctx.params.id));
+    ctx.body = await ctx.service.course.find(ctx.helper.parseInt(ctx.params.id));
   }
 
-  async create() {
+  async getCourseBySpecialColumnId(){
     const ctx = this.ctx;
-    const courseType = await ctx.service.courseType.create(ctx.request.body);
-    ctx.body = ctx.app.success('创建成功!');
+    const query = {
+      limit: ctx.helper.parseInt(ctx.query.limit),
+      offset: ctx.helper.parseInt(ctx.query.offset),
+      id: ctx.params.id,
+    };
+    ctx.body = await ctx.service.course.getCourseBySpecialColumnId(query);
+  }
+
+  async getCourseByCourseTypeId(){
+    const ctx = this.ctx;
+    const query = {
+      limit: ctx.helper.parseInt(ctx.query.limit),
+      offset: ctx.helper.parseInt(ctx.query.offset),
+      id: ctx.params.id,
+    };
+    ctx.body = await ctx.service.course.getCourseByCourseTypeId(query);
+  }
+
+  async getCourseByCondition(){
+    const ctx = this.ctx;
+    const query = {
+      limit: ctx.helper.parseInt(ctx.query.limit),
+      offset: ctx.helper.parseInt(ctx.query.offset),
+      courseType: ctx.helper.parseInt(ctx.query.courseType),
+      specialColumn: ctx.helper.parseInt(ctx.query.specialColumn),
+    };
+    ctx.body = await ctx.service.course.getCourseByCondition(query);
   }
 
 }
 
-module.exports = CourseTypeController;
+module.exports = CourseController;
