919bbb4f0c42e1c899abb730735b78e17b80efbe
gansiqiu <1223720954@qq.com>
2019-03-21 17:38:43 +0800

    新版本名校试卷

---
+++ PrometheusWxApplet/pages/recommend/recommend.js
@@ -0,0 +1,83 @@
+// pages/recommend/recommend.js
+Page({
+
+   /**
+    * 页面的初始数据
+    */
+   data: {
+      currentPage:"1",
+      totalPage:"10",
+      dataList:[
+         "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+         "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+         "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+         "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"
+      ]
+   },
+   // cardSwiper
+   swiperChange(event) {
+      console.log(event)
+      let currentPage = event.detail.current;
+      this.setData({
+         currentPage: currentPage + 1
+      })
+   },
+   catchItem(event){
+      console.log(event)
+   },
+   /**
+    * 生命周期函数--监听页面加载
+    */
+   onLoad: function (options) {
+
+   },
+
+   /**
+    * 生命周期函数--监听页面初次渲染完成
+    */
+   onReady: function () {
+
+   },
+
+   /**
+    * 生命周期函数--监听页面显示
+    */
+   onShow: function () {
+
+   },
+
+   /**
+    * 生命周期函数--监听页面隐藏
+    */
+   onHide: function () {
+
+   },
+
+   /**
+    * 生命周期函数--监听页面卸载
+    */
+   onUnload: function () {
+
+   },
+
+   /**
+    * 页面相关事件处理函数--监听用户下拉动作
+    */
+   onPullDownRefresh: function () {
+
+   },
+
+   /**
+    * 页面上拉触底事件的处理函数
+    */
+   onReachBottom: function () {
+
+   },
+
+   /**
+    * 用户点击右上角分享
+    */
+   onShareAppMessage: function () {
+
+   }
+})
